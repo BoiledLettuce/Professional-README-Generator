4983f5f3b074b1435e9b8c24caa58ce0cb854940
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-07-19 22:10:58 -0700

    created functionality for prompts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -52,48 +54,56 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'repository',
-        message: 'What is your repos name?',
+        name: 'test',
+        message: 'What is test instructions?',
         validate: (value) => {
-            if (value) { return true } else { return 'Enter your repos name...'}
+            if (value) { return true } else { return 'Enter test instructions...'}
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What project license?',
+        choices: ["MIT", "ISC", "GNU GPLv3"],
+        validate: (value) => {
+            if (value) { return true } else { return 'Choose a license...'}
         }
     },
     {
         type: 'input',
-        name: 'repository',
-        message: 'What is your repos name?',
+        name: 'github_name',
+        message: 'What is your GitHub gamertag?',
         validate: (value) => {
-            if (value) { return true } else { return 'Enter your repos name...'}
+            if (value) { return true } else { return 'Enter your username...'}
         }
     },
     {
         type: 'input',
-        name: 'repository',
-        message: 'What is your repos name?',
+        name: 'github_email',
+        message: 'What is email address for github?',
         validate: (value) => {
-            if (value) { return true } else { return 'Enter your repos name...'}
+            if (value) { return true } else { return 'Enter an email...'}
         }
     },
-    {
-        type: 'input',
-        name: 'repository',
-        message: 'What is your repos name?',
-        validate: (value) => {
-            if (value) { return true } else { return 'Enter your repos name...'}
-        }
-    },
-
-
-
-
-
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {  
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('README success');
+        }
+    })
+ };
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {   
+    inquirer.prompt(questions).then(function (data) {
+        writeToFile("README.md", generateMarkdown(data));
+    })
+ };
 
 // Function call to initialize app
 init();
