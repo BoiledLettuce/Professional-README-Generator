d1e457af358d2e8b1fb75e12284c3be36d5187c0
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-07-21 21:14:32 -0700

    added functionality to generatemarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,52 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ![License Badge](https://img.shields.io/github/license${data.github}/${data.repo})
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [License](#license)
+  
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+
+  * Take a look at my [Github profile](https://github.com/${data.github})
+
+  * If you would like to contact me, [send an email](mailto:${data.email}).
+
+  ## License
+
+  Copyright (c) [${data.github}](https://github.com/${data.github}). All rights reserved.
+
+  Licensed under the ${data.license} license.
+
+  
 `;
 }
 
